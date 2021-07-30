80f361d2ad1ffff55b2a52e832f63a92c3830b39
Alyeev <markmarkul123@gmail.com>
2021-07-30 19:57:51 +0400

    Add expo needs

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Signal</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Signal</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHNoJz7ojc0T-6OiNz1mNJHblVR_x9JUY",
+  authDomain: "signal-chat-7c38a.firebaseapp.com",
+  projectId: "signal-chat-7c38a",
+  storageBucket: "signal-chat-7c38a.appspot.com",
+  messagingSenderId: "683866199647",
+  appId: "1:683866199647:web:8b80e4a7efa301b91fadea",
+};
